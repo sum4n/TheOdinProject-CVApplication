44e95873c713933b0550945abdde5a015093c4a0
Suman Das <sum4ndas@gmail.com>
2023-09-11 09:26:52 +0530

    Add general info component with basic structure and style

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,16 @@
+import "../styles/style.css";
+
+function GeneralInfo() {
+  return (
+    <div className="info-box">
+      <h2>General Info</h2>
+      {/* <hr /> */}
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default GeneralInfo;
