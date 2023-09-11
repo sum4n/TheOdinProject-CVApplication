7a83f3607414bf3cd677d04bea661460daa02f35
Suman Das <sum4ndas@gmail.com>
2023-09-11 19:44:39 +0530

    Make GeneralInfo component work according to project instruction

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,14 +1,70 @@
+import { useState } from "react";
 import "../styles/style.css";
 
 function GeneralInfo() {
+  const [inititalInfo, setInfo] = useState({ name: "", email: "", phone: "" });
+
+  const [showForm, setShowForm] = useState(true);
+
+  function handleChange(e) {
+    setInfo({
+      ...inititalInfo,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit() {
+    setShowForm(!showForm);
+  }
+
   return (
     <div className="info-box">
       <h2>General Info</h2>
-      {/* <hr /> */}
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <button>Submit</button>
+
+      {showForm ? (
+        <div className="info-content">
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={inititalInfo.name}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={inititalInfo.email}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Phone:
+            <input
+              type="text"
+              name="phone"
+              value={inititalInfo.phone}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      ) : (
+        <div className="info-content">
+          <p>Name: {inititalInfo.name}</p>
+          <p>Email: {inititalInfo.email}</p>
+          <p>Phone: {inititalInfo.phone}</p>
+          <button onClick={handleSubmit}>Edit</button>
+        </div>
+      )}
     </div>
   );
 }
