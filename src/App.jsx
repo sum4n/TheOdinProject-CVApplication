2d4c7756cde281f0f384a818516c9ab9e1726ac8
Suman Das <sum4ndas@gmail.com>
2023-11-04 07:45:36 +0530

    Move components import into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import GeneralInfo from "./components/GeneralInfo.jsx";
+import EducationExp from "./components/EducationExp.jsx";
+import PracticalExp from "./components/PracticalExp.jsx";
+
 function App() {
-  return <h1>CV Application</h1>;
+  return (
+    <>
+      <h1>CV Application</h1>
+      <GeneralInfo />
+      <EducationExp />
+      <PracticalExp />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import GeneralInfo from "./components/GeneralInfo.jsx";
-import EducationExp from "./components/EducationExp.jsx";
-import PracticalExp from "./components/PracticalExp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <GeneralInfo />
-    <EducationExp />
-    <PracticalExp />
   </React.StrictMode>
 );
